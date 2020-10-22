8e357ddba198cd6297896f30fc9f2b0ac27a22b0
vshao288 <shao_vivian@cl.fcasd.edu>
2020-10-22 18:58:35 -0400

    vivian's codelab part1

---
+++ gitflow/src/main/java/frc/robot/subsystems/VivianCodelab2020Part1Subsystem.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VivianCodelab2020Part1Subsystem extends SubsystemBase {
+    public VivianCodelab2020Part1Subsystem() {
+        System.out.println("Vivian Says Hello World in 2020, part 1");
+    }
+}
